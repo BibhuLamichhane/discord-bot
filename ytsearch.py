fe15359ae2612bcdb676b200935cde0f1411e301
BibhuLamichhane <bibhulamichhane0@gmail.com>
2020-06-05 20:06:23 +0545

    returns url for search results

---
+++ ytsearch.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+client_secrets_file = "client secret.json"
+api_service_name = "youtube"
+api_version = "v3"
+
+def main(url):
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    youtube = get_authenticated_service()
+    request = youtube.search().list(
+        part="snippet",
+        maxResults=2,
+        q= url
+    )
+    response = request.execute()
+    a = response
+    return a['items'][0]['id']['videoId']
+
+def get_authenticated_service():
+    if os.path.exists("CREDENTIALS_PICKLE_FILE"):
+        with open("CREDENTIALS_PICKLE_FILE", 'rb') as f:
+            credentials = pickle.load(f)
+    else:
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+        credentials = flow.run_console()
+        with open("CREDENTIALS_PICKLE_FILE", 'wb') as f:
+            pickle.dump(credentials, f)
+    return googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
